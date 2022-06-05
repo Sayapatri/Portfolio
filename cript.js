d069b2b4b956b8b04be3f055fc152748f772abb8
kapilz007 <sthakapil007@gmail.com>
2022-06-05 12:19:22 +0545

    kkk

--- cript.js
+++ cript.js
@@ -1,136 +1,144 @@
-var mouseX = 0,
-      mouseY = 0,
-      windowHalfX = window.innerWidth / 2,
-      windowHalfY = window.innerHeight / 2,
-      SEPARATION = 200,
-      AMOUNTX = 10,
-      AMOUNTY = 10,
-      camera,
-      scene,
-      renderer;
-  
-  init();
-  animate();
+window.requestAnimFrame = function()
+	{
+		return (
+			window.requestAnimationFrame       || 
+			window.webkitRequestAnimationFrame || 
+			window.mozRequestAnimationFrame    || 
+			window.oRequestAnimationFrame      || 
+			window.msRequestAnimationFrame     || 
+			function(/* function */ callback){
+				window.setTimeout(callback, 1000 / 60);
+			}
+		);
+}();
 
-	function init() {
+var canvas = document.getElementById('canvas'); 
+var context = canvas.getContext('2d');
 
-  	var container,
-        separation = 100,
-        amountX = 50,
-        amountY = 50,
-        particle;
-    
-    container = document.createElement( 'div' );
-    document.body.appendChild( container );
+//get DPI
+let dpi = window.devicePixelRatio || 1;
+context.scale(dpi, dpi);
+console.log(dpi);
 
-    scene = new THREE.Scene();
+function fix_dpi() {
+//get CSS height
+//the + prefix casts it to an integer
+//the slice method gets rid of "px"
+let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
 
-    renderer = new THREE.CanvasRenderer({ alpha: true }); // gradient; this can be swapped for WebGLRenderer
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    container.appendChild( renderer.domElement );
-    
-    camera = new THREE.PerspectiveCamera(
-    	75,
-      window.innerWidth / window.innerHeight,
-      1,
-      10000
-    );
-    camera.position.z = 100;
+//scale the canvas
+canvas.setAttribute('height', style_height * dpi);
+canvas.setAttribute('width', style_width * dpi);
+}
 
-		// particles
-    var PI2 = Math.PI * 2;
-    var material = new THREE.SpriteCanvasMaterial({
-    	color: 0xffffff,
-    	program: function ( context ) {
-				context.beginPath();
-        context.arc( 0, 0, 0.5, 0, PI2, true );
-        context.fill();
-      }
-    });
-    
-    var geometry = new THREE.Geometry();
+	var particle_count = 70,
+		particles = [],
+		couleurs   = ["#3a0088", "#930077", "#e61c5d","#ffbd39"];
+    function Particle()
+    {
 
-    for ( var i = 0; i < 100; i ++ ) {
-      particle = new THREE.Sprite( material );
-      particle.position.x = Math.random() * 2 - 1;
-      particle.position.y = Math.random() * 2 - 1;
-      particle.position.z = Math.random() * 2 - 1;
-      particle.position.normalize();
-      particle.position.multiplyScalar( Math.random() * 10 + 450 );
-      particle.scale.x = particle.scale.y = 10;
-      scene.add( particle );
-      geometry.vertices.push( particle.position );
+        this.radius = Math.round((Math.random()*3)+5);
+        this.x = Math.floor((Math.random() * ((+getComputedStyle(canvas).getPropertyValue("width").slice(0, -2) * dpi) - this.radius + 1) + this.radius));
+        this.y = Math.floor((Math.random() * ((+getComputedStyle(canvas).getPropertyValue("height").slice(0, -2) * dpi) - this.radius + 1) + this.radius));
+        this.color = couleurs[Math.floor(Math.random()*couleurs.length)];
+        this.speedx = Math.round((Math.random()*201)+0)/100;
+        this.speedy = Math.round((Math.random()*201)+0)/100;
+
+        switch (Math.round(Math.random()*couleurs.length))
+        {
+            case 1:
+            this.speedx *= 1;
+            this.speedy *= 1;
+            break;
+            case 2:
+            this.speedx *= -1;
+            this.speedy *= 1;
+            break;
+            case 3:
+            this.speedx *= 1;
+            this.speedy *= -1;
+            break;
+            case 4:
+            this.speedx *= -1;
+            this.speedy *= -1;
+            break;
+        }
+            
+        this.move = function()
+        {
+            
+            context.beginPath();
+            context.globalCompositeOperation = 'source-over';
+            context.fillStyle   = this.color;
+            context.globalAlpha = 1;
+            context.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+            context.fill();
+            context.closePath();
+
+            this.x = this.x + this.speedx;
+            this.y = this.y + this.speedy;
+            
+            if(this.x <= 0+this.radius)
+            {
+                this.speedx *= -1;
+            }
+            if(this.x >= canvas.width-this.radius)
+            {
+                this.speedx *= -1;
+            }
+            if(this.y <= 0+this.radius)
+            {
+                this.speedy *= -1;
+            }
+            if(this.y >= canvas.height-this.radius)
+            {
+                this.speedy *= -1;
+            }
+
+            for (var j = 0; j < particle_count; j++)
+            {
+                var particleActuelle = particles[j],
+                    yd = particleActuelle.y - this.y,
+                    xd = particleActuelle.x - this.x,
+                    d  = Math.sqrt(xd * xd + yd * yd);
+
+                if ( d < 200 )
+                {
+                    context.beginPath();
+                    context.globalAlpha = (200 - d) / (200 - 0);
+                    context.globalCompositeOperation = 'destination-over';
+                    context.lineWidth = 1;
+                    context.moveTo(this.x, this.y);
+                    context.lineTo(particleActuelle.x, particleActuelle.y);
+                    context.strokeStyle = this.color;
+                    context.lineCap = "round";
+                    context.stroke();
+                    context.closePath();
+                }
+            }
+        };
+    };
+    for (var i = 0; i < particle_count; i++)
+    {
+        fix_dpi();
+        var particle = new Particle();
+        particles.push(particle);
     }
 
-    // lines
-    var line = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 0.5 } ) );
-    scene.add( line );
 
-    // mousey
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-		document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
-		
- 		window.addEventListener( 'resize', onWindowResize, false );
-
-	} // end init();
-
-	function onWindowResize() {
-
-  	windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-	}
-
-	function onDocumentMouseMove(event) {
-
-  	mouseX = event.clientX - windowHalfX;
-    mouseY = event.clientY - windowHalfY;
-
-  }
-
-  function onDocumentTouchStart( event ) {
-
-		if ( event.touches.length > 1 ) {
-
-    	event.preventDefault();
-
-      mouseX = event.touches[ 0 ].pageX - windowHalfX;
-      mouseY = event.touches[ 0 ].pageY - windowHalfY;
-
+    function animate()
+    {
+        fix_dpi();
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        for (var i = 0; i < particle_count; i++)
+        {
+            particles[i].move();
+        }
+        requestAnimFrame(animate);
     }
-	}
-
-  function onDocumentTouchMove( event ) {
-
-  	if ( event.touches.length == 1 ) {
-
-    	event.preventDefault();
-
-      mouseX = event.touches[ 0 ].pageX - windowHalfX;
-      mouseY = event.touches[ 0 ].pageY - windowHalfY;
-
-		}
-	}
-
-	function animate() {
     
-		requestAnimationFrame( animate );
-    render();
     
-	}
 
-	function render() {
-		
-    camera.position.x += ( mouseX - camera.position.x ) * .05;
-    camera.position.y += ( - mouseY + 200 - camera.position.y ) * .05;
-    camera.lookAt( scene.position );
-
-    renderer.render( scene, camera );
-  
-  }
+   
+    animate(); 
