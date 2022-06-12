3b0f3ba7b742f48de0a3a3dcefa1b93066291bf5
kapilz007 <sthakapil007@gmail.com>
2022-06-12 12:38:10 +0545

    almost

--- cript.js
+++ cript.js
@@ -16,3 +16,12 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('active');
 };
+const sr = ScrollReveal({
+    distance: '45px',
+    duration: 2700,
+    reset: true
+
+})
+sr.reveal('.home-text', {delay:350, origin:'left'})
+sr.reveal('.home-img', {delay:350, origin:'right'})
+sr.reveal('.sub-service,.about,.blogs,.projects,.cta,.contact', {delay:20, origin:'bottom'})
